91a69d7bb91c8a3b7fefffa105f70f6cfcea8fdb
sudora1n <me@sudora1n.dev>
2025-05-30 23:04:35 +0300

    fix amount->star_count field

--- types.go
+++ types.go
@@ -4343,7 +4343,7 @@ type AcceptedGiftTypes struct {
 // StarAmount - Describes an amount of Telegram Stars.
 type StarAmount struct {
 	// Amount - Integer amount of Telegram Stars, rounded to 0; can be negative
-	Amount int `json:"amount"`
+	Amount int `json:"star_count"`
 
 	// NanostarAmount - Optional. The number of 1/1000000000 shares of Telegram Stars; from -999999999 to
 	// 999999999; can be negative if and only if amount is non-positive
